231bf16484bc456e9d11b768985efeecff2f7c88
MinJeong-Jang <zena4649@gmail.com>
2022-10-18 23:10:08 +0900

    update student20191009

--- HW2/student20191009.py
+++ HW2/student20191009.py
@@ -5,6 +5,7 @@ import openpyxl
 wb = openpyxl.load_workbook("student.xlsx")
 ws = wb['Sheet1']
 
+#총점 계산
 row_id = 1
 total = []
 for row in ws:
@@ -21,19 +22,27 @@ for row in ws:
 totalList = sorted(total, key = lambda x:x[1], reverse = True)
 students = len(total)
 
-#순차적으로 grade 설정
-for i in range(students):
-	ws.cell(row = totalList[i][0], column = 8).value = 'C0'
-for i in range(int(students * 0.85)):
-	ws.cell(row = totalList[i][0], column = 8).value = 'C+'
-for i in range(int(students * 0.7)):
-	ws.cell(row = totalList[i][0], column = 8).value = 'B0'
-for i in range(int(students * 0.5)):
-	ws.cell(row = totalList[i][0], column = 8).value = 'B+'
-for i in range(int(students * 0.3)):
+#학점 범위 설정
+AZ = int(students * 0.3)
+AP = int(AZ * 0.5)
+BZ = int(students * 0.7) - AZ
+BP = int(BZ * 0.5)
+CZ = students - AZ - BZ
+CP = int(CZ * 0.5)
+
+#순차적으로 성적에 따라 grade 설정
+for i in range(AZ):
 	ws.cell(row = totalList[i][0], column = 8).value = 'A0'
-for i in range(int(students * 0.15)):
+for i in range(AP):
 	ws.cell(row = totalList[i][0], column = 8).value = 'A+'
+for i in range(AZ, AZ + BZ):
+	ws.cell(row = totalList[i][0], column = 8).value = 'B0'
+for i in range(AZ, AZ + BP):
+	ws.cell(row = totalList[i][0], column = 8).value = 'B+'
+for i in range(AZ + BZ, AZ + BZ + CZ):
+	ws.cell(row = totalList[i][0], column = 8).value = 'C0'
+for i in range(AZ + BZ, AZ + BZ + CP):
+	ws.cell(row = totalList[i][0], column = 8).value = 'C+'
 
 wb.save("student.xlsx")
 
