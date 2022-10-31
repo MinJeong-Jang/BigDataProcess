902b4b1b3d1ca177039dbd88102838e0044a3c9d
MinJeong-Jang <zena4649@gmail.com>
2022-10-31 22:56:21 +0900

    update HW3

--- HW3/UBERStudent20191009.py
+++ HW3/UBERStudent20191009.py
@@ -11,6 +11,7 @@ uberDic = dict()
 with open(inputfile, "rt") as fp:
 	for line in fp:
 		uber = line.split(",")
+
 		dayList = uber[1].split("/")
 		day = calendar.weekday(int(dayList[2]), int(dayList[0]), int(dayList[1]))
 
@@ -23,7 +24,7 @@ with open(inputfile, "rt") as fp:
 			value = uberDic[key].split(",")
 			vehicle += int(value[0])
 			trip += int(value[1])
-		uberDic = str(vehicle) + "," + str(trip)
+		uberDic[key] = str(vehicle) + "," + str(trip)
 
 with open(outputfile, "wt") as fp:
 	items = uberDic.items()
