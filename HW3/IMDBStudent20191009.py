af8d554eae7cec19ab7ac89355b3f96d3e07f3c6
MinJeong-Jang <zena4649@gmail.com>
2022-10-31 17:26:02 +0900

    push HW3

---
+++ HW3/IMDBStudent20191009.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import sys
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+genreDic = dict{}
+
+with open(inputFile, "rt") as fp:
+	for line in fp:
+		movie = line.split("::")
+		genreList = movie[2].strip().split("|")
+
+		for genre in genreList:
+			if genre not in countGenre:
+				genreDic[genre] = 1
+			else:
+				genreDic[genre] += 1
+
+with open(outputFile, "wt") as fp:
+	items = genreDic.items()
+	for item in items:
+		fp.write(item[0] + " " + str(item[1]) + "\n")
---
+++ HW3/UBERStudent20191009.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sys
+import calendar
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+dayOfWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+
+uberDic = dict{}
+with open(inputFile, "rt") as fp:
+	for line in fp:
+		uber = line.split(",")
+		dayList = uber[1].split("/")
+		day = calendar.weekday(int(dayList[2]), int(dayList[0]), int(dayList[1]))
+
+		n = uber[0]
+		vehicle = int(uber[2])
+		trip = int(uber[3][:-1])
+		key = n + "," + dayOfWeek[day]
+
+		if key in uberDic:
+			value = uberDic[key].split(",")
+			vehicle += int(value[0])
+			trip += int(value[1])
+		uberDic = str(vehicle) + "," + str(trip)
+
+with open(outputFile, "wt") as fp:
+	items = uberDic.items()
+	for item in items:
+		fp.write(item[0] + " " + str(item[1]) + "\n")
+		
+
+		
